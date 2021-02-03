8174fa3ce68490c9228a4bbb54eb1d4d3e5690d4
FaraonDaniel <dacandil@ucm.es>
2021-02-03 20:05:54 +0100

    webConfig

--- empleos/src/main/java/net/itinajero/config/WebConfig.java
+++ empleos/src/main/java/net/itinajero/config/WebConfig.java
@@ -12,8 +12,6 @@ public class WebConfig implements WebMvcConfigurer{
 	private String rutaImagenes;
 
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		//registry.addResourceHandler("/logos/**").addResourceLocations("file:/empleos/img-vacantes/"); // Linux
-		//registry.addResourceHandler("/logos/**").addResourceLocations("file:c:/empleos/img-vacantes/"); // Windows
 		registry.addResourceHandler("/logos/**").addResourceLocations("file:" + rutaImagenes); // Windows
 	}
 }
