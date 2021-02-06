e00a0f6c643e1afc00a7342e29b2e60a3aa547e3
FaraonDaniel <dacandil@ucm.es>
2021-02-06 12:58:17 +0100

    modification to pretty view

--- empleos/src/main/java/net/itinajero/security/DatabaseWebSecurity.java
+++ empleos/src/main/java/net/itinajero/security/DatabaseWebSecurity.java
@@ -1,44 +1,80 @@
 package net.itinajero.security;
 
 import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class DatabaseWebSecurity extends WebSecurityConfigurerAdapter {
+
 	@Autowired
 	private DataSource dataSource;
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
 		auth.jdbcAuthentication().dataSource(dataSource)
-				.usersByUsernameQuery("select username, password, estatus from Usuarios where username=?")
-				.authoritiesByUsernameQuery("select u.username, p.perfil from UsuarioPerfil up "
-						+ "inner join Usuarios u on u.id = up.idUsuario "
-						+ "inner join Perfiles p on p.id = up.idPerfil " + "where u.username = ?");
+		.usersByUsernameQuery("select username, password, estatus from Usuarios where username=?")
+		.authoritiesByUsernameQuery("select u.username, p.perfil from UsuarioPerfil up " + 
+			"inner join Usuarios u on u.id = up.idUsuario " + 
+			"inner join Perfiles p on p.id = up.idPerfil " + 
+			"where u.username = ?");
 	}
 
+	/**
+	 * Personalizamos el Acceso a las URLs de la aplicaci贸n
+	 */
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-				// Los recursos est醫icos no requieren autenticaci髇
-				.antMatchers("/bootstrap/**", "/images/**", "/tinymce/**", "/logos/**").permitAll()
-				// Las vistas p鷅licas no requieren autenticaci髇
-				.antMatchers("/", "/signup", "/search", "/vacantes/view/**").permitAll()
-				// Asignar permisos a URLs por ROLES
-				.antMatchers("/vacantes/**").hasAnyAuthority("SUPERVISOR","ADMINISTRADOR")
-				.antMatchers("/categorias/**").hasAnyAuthority("SUPERVISOR","ADMINISTRADOR")
-				.antMatchers("/usuarios/**").hasAnyAuthority("ADMINISTRADOR")
-				// Todas las dem醩 URLs de la Aplicaci髇 requieren autenticaci髇
-				.anyRequest().authenticated()
-				// El formulario de Login no requiere autenticacion
-				.and().formLogin().permitAll();
+        http.authorizeRequests() 
+            	
+    	// Los recursos est谩ticos no requieren autenticaci贸n
+        .antMatchers(
+                "/bootstrap/**",                        
+                "/images/**",
+                "/tinymce/**",
+                "/logos/**").permitAll()
+        
+        // Las vistas p煤blicas no requieren autenticaci贸n
+        .antMatchers("/", 
+        			 "/login",
+        			 "/signup",
+        			 "/search",
+        			 "/bcrypt/**",
+        			 "/about",
+        			 "/vacantes/view/**").permitAll()
+        
+        // Asignar permisos a URLs por ROLES
+        .antMatchers("/solicitudes/create/**",
+        			 "/solicitudes/save/**").hasAuthority("USUARIO")
+        
+        .antMatchers("/solicitudes/**").hasAnyAuthority("SUPERVISOR","ADMINISTRADOR")
+        .antMatchers("/vacantes/**").hasAnyAuthority("SUPERVISOR","ADMINISTRADOR")
+        .antMatchers("/categorias/**").hasAnyAuthority("SUPERVISOR","ADMINISTRADOR")
+        .antMatchers("/usuarios/**").hasAnyAuthority("ADMINISTRADOR")
+        
+        // Todas las dem谩s URLs de la Aplicaci贸n requieren autenticaci贸n
+        .anyRequest().authenticated()
+        // El formulario de Login no requiere autenticacion
+        .and().formLogin().loginPage("/login").permitAll()        
+        .and().logout().permitAll();
+    }
+	
+	/**
+	 *  Implementaci贸n de Spring Security que encripta passwords con el algoritmo Bcrypt
+	 * @return
+	 */
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+	    return new BCryptPasswordEncoder();
 	}
 
 }
